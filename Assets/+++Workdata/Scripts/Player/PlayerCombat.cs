0b9ad6d27cf8e48ec500ba612a5405d6151efc1a
Diana Weber <dianaweber1902@gmail.com>
2022-09-23 16:24:50 +0200

    Built ComboAttack + HeavyAttack mechanics, added Inventory and Hotbar death awaits us all

--- Assets/+++Workdata/Scripts/Player/PlayerCombat.cs
+++ Assets/+++Workdata/Scripts/Player/PlayerCombat.cs
@@ -6,6 +6,9 @@ public class PlayerCombat : MonoBehaviour
 {
     PlayerController playerController;
     PlayerInfos playerInfos;
+    public int attackState;
+    public float attackTimer, attackTimerInit, chargingTimer, chargingTimerGoal;
+    public bool isAttacking, isCharging, attackReleased;
     void Start()
     {
         playerController = gameObject.GetComponent<PlayerController>();
@@ -14,15 +17,114 @@ public class PlayerCombat : MonoBehaviour
 
     void Update()
     {
+        if(attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+            isAttacking = true;
+            playerController.anim.SetBool("isAttacking", isAttacking);
+        }
+        else 
+        {
+            attackState = 0;
+            playerController.anim.SetInteger("attackState", attackState);
+        }
 
+        if(!attackReleased && isAttacking)
+            {
+                isCharging = true;
+                chargingTimer += Time.deltaTime;
+
+                if(chargingTimer > chargingTimerGoal)
+                {
+                    HeavyAttack();
+                }
+            }
+        else
+            isCharging = false;
     }
     public void Attack()
     {
-        
+        if(playerInfos.inventoryState == 0)
+        {
+            if(attackState == 0)
+            {
+                attackTimer = attackTimerInit;
+                chargingTimer = 0;
+                Debug.Log("Attack 1");   
+            }
+            else if(attackState == 1)
+            {
+                AttackFollowup1();
+            }
+            else if(attackState == 2)
+            {
+                AttackFollowup2();
+            }
+
+            if(!attackReleased && attackTimer < 0)
+            {
+                isCharging = true;
+                isAttacking = true;
+                attackReleased = false;
+            }
+            else if (attackReleased)
+            {
+                isAttacking = false;
+                attackReleased = true;   
+            }
+            attackReleased = false;
+                
+        }
+        else 
+        {
+            Debug.Log("Ranged weapon equipped");
+            //Shooting stuff
+        }
+    }
+
+    void AttackFollowup1()
+    {
+        attackTimer = attackTimerInit;
+        chargingTimer = 0;
+        Debug.Log("Attack 2");
     }
     
+    void AttackFollowup2()
+    {
+        attackTimer = attackTimerInit;
+        attackState = -1;
+        chargingTimer = 0;
+        Debug.Log("Attack 3");
+    }
+
     public void AttackRelease()
     {
-        
+        if(playerInfos.inventoryState == 0)
+        {
+            if(attackTimer > 0)
+            {
+                attackState++;
+                playerController.anim.SetInteger("attackState", attackState);
+            }
+
+            attackReleased = true;
+            isCharging = false;
+            StopAttacking();
+        }
+    }
+
+    public void HeavyAttack()
+    {
+        isCharging = false;
+        chargingTimer = 0;
+        StopAttacking();
+
+        Debug.Log("Heavy attack");
+    }
+
+    public void StopAttacking()
+    {
+        isAttacking = false;
+        playerController.anim.SetBool("isAttacking", isAttacking);
     }
 }
